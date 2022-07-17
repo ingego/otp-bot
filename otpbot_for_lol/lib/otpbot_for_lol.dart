f6622c713819924687522d509f27ff5ed521f378
Nikita <niskirzi@gmail.com>
2022-07-17 16:55:07 +0300

    Settings+
    Settings+

--- otpbot_for_lol/lib/otpbot_for_lol.dart
+++ otpbot_for_lol/lib/otpbot_for_lol.dart
@@ -2,12 +2,15 @@ import 'dart:math';
 
 import 'package:logger/logger.dart';
 import 'package:otpbot_for_lol/otpbot_for_lol.dart';
+import 'package:otpbot_for_lol/parsers.dart';
 import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:test/test.dart';
 
 void main()async {
+  final ParserOfSummonerData summoner = ParserOfSummonerData();
+  final FavChampsFromOPGG summonerFavCh = FavChampsFromOPGG();
 const token = '5575781544:AAE5QrKeVNRowZR1vSp7QMbpnx9Y7u55LS4';
 final username = (await Telegram(token).getMe()).username;
   var teledart = TeleDart(token, Event(username!));
@@ -21,8 +24,16 @@ teledart
     .listen((message) => message.reply('Приветствую, Призыватель! Я создан , что бы помогат выбирать OTP персонажа для LOL, исходя из твоей роли, эло и персонажей, на которых ты играешь! Введи свой ник...'));
 teledart 
   .onMessage()
-  .listen((event){String? usermessage=(event.text ?? '');event.reply("Ну $usermessage и пидорас, конечно!!");})
-
-
-  
+  .listen((event)async{String? usermessage=(event.text ?? '');
+  await summoner.envSetUp(usermessage, "ru");
+  await summonerFavCh.envSetUp(usermessage, "ru");
+  event.reply(summoner.leagueTier()!);
+  event.reply(summoner.positionInGame()!);
+  String messagList="Ваши персонажи:";
+  for (var value in summonerFavCh.favChamps()){
+    messagList+="\n$value \n";
+  };
+event.reply(messagList);
+  // event.reply(summonerFavCh.favChamps()!);
+  })
 ;}
---
+++ otpbot_for_lol/lib/parsers.dart
@@ -0,0 +1,49 @@
+
+
+
+import 'package:beautiful_soup_dart/beautiful_soup.dart';
+import 'package:dio/dio.dart';
+
+class ParserOfSummonerData{
+
+String? url;
+Response? resp;
+
+Future <void> envSetUp(String name, String locale)async{url = "https://www.leagueofgraphs.com/summoner/$locale/$name";
+      resp = await Dio().get(url!);}
+String? leagueTier(){
+
+BeautifulSoup sp = BeautifulSoup(resp!.data);
+          var tierSQ = sp.find("div", class_: "leagueTier")?.getText();
+                return(tierSQ);
+
+}
+
+String? positionInGame(){
+
+BeautifulSoup sp = BeautifulSoup(resp!.data);
+          var linePosition = sp.find("div", id: "profileRoles")?.
+          find("div", class_: "tabs-content")?.find("div", class_:"content active")?.find("div", class_: "txt name")?.getText();
+            return(linePosition);
+
+}
+}
+
+class FavChampsFromOPGG{
+
+String? url;
+Response? resp;
+
+Future <void> envSetUp(String name, String locale)async{url = "https://ru.op.gg/summoners/$locale/$name";
+      resp = await Dio().get(url!);}
+List <String?> favChamps(){
+List <String?> champs = [];
+BeautifulSoup sp = BeautifulSoup(resp!.data);
+          var champList = sp.find("div", class_: "css-e9xk5o e1g7spwk3")?.findAll("div", class_: "name");
+              for (var value in champList!){
+                champs.add((value).getText());
+                
+                }
+ return champs;
+}
+}
