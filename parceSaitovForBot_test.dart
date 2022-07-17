17b2d39d721239c6eec0b2fc309a286b2d2e859b
DrPechenyshka <109425770+DrPechenyshka@users.noreply.github.com>
2022-07-17 12:15:08 +0300

    Add files via upload

--- parceSaitovForBot_test.dart
+++ parceSaitovForBot_test.dart
@@ -10,9 +10,9 @@ void main() {
   Response? resp;
   
 
-  group("parser", (){
+  group("parserOfSummonerData", (){
     setUp(() async {
-      url = r"https://www.leagueofgraphs.com/summoner/ru/TiPidorina";
+      url = r"https://www.leagueofgraphs.com/summoner/ru/CopyBara";
       resp = await Dio().get(url!);
       });
 
@@ -34,18 +34,31 @@ void main() {
           find("div", class_: "tabs-content")?.find("div", class_:"content active")?.find("div", class_: "txt name")?.getText();
             Logger().w(linePosition);
         });
-        test("favChamps", (){
-          BeautifulSoup sp = BeautifulSoup(resp!.data);
-          var favChamps = sp.find("div", id: "favchamps")?.find("div", class_: "tabs-content")
-          ?.find("div", class_: "content active")?.findAll("div", class_: "name");
-          for (var value in favChamps!){
-            Logger().d((value).getText());
-
-          }
-
-        });
       });
  });
+  group("favChampsFromOPGG", (){
+    setUp(() async {
+      url = r"https://ru.op.gg/summoners/ru/CopyBara";
+      resp = await Dio().get(url!);
+      });
+      group("Unit Test", (){
 
+         test('setUp', (){
+          expect(url, url!);
+          expect(resp, resp!);
+        });
+
+         test("Parse favChamps", (){
+          BeautifulSoup sp = BeautifulSoup(resp!.data);
+          var champList = sp.find("div", class_: "css-e9xk5o e1g7spwk3")?.findAll("div", class_: "name");
+              for (var value in champList!){
+                Logger().w((value).getText());
+                }
+                
+              });
+
+      });
+
+  });
 
 }
