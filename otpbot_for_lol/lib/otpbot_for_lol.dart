ed7a9681341af8e0e57c9c4321121abd772852fe
Nikita <niskirzi@gmail.com>
2022-07-17 12:24:52 +0300

    Исправление ошибок (сыроправление)
    Исправление ошибок Ссыроправление)

--- otpbot_for_lol/lib/otpbot_for_lol.dart
+++ otpbot_for_lol/lib/otpbot_for_lol.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:logger/logger.dart';
 import 'package:otpbot_for_lol/otpbot_for_lol.dart';
 import 'package:teledart/model.dart';
@@ -10,12 +12,17 @@ const token = '5575781544:AAE5QrKeVNRowZR1vSp7QMbpnx9Y7u55LS4';
 final username = (await Telegram(token).getMe()).username;
   var teledart = TeleDart(token, Event(username!));
   teledart.start();
-
-   teledart
+ 
+  // var telegram = Telegram(token); 2 варианат включения бота
+  // var event = Event((await telegram.getMe()).username!);
+  // final bot = TeleDart(token, event);
+teledart
     .onCommand("start")
-    .listen((message) => message.reply('Приветствую, Призыватель! Я создан , что бы помогат выбирать OTP персонажа для LOL, исходя из твоей роли, эло и персонажей, на которых ты играешь! Введи свой ник'));
-  teledart
+    .listen((message) => message.reply('Приветствую, Призыватель! Я создан , что бы помогат выбирать OTP персонажа для LOL, исходя из твоей роли, эло и персонажей, на которых ты играешь! Введи свой ник...'));
+teledart 
+  .onMessage()
+  .listen((event){String? usermessage=(event.text ?? "Пустое место? Рили?!");event.reply("Ну $usermessage и пидорас, конечно!!");})
+//  .listen((event){String? usermessage=(event.text ?? "Пустое место? Рили?!");})
 
   
-    
 ;}
